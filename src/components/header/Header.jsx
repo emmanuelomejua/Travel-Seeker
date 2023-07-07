2cff9b8567d6c1648761d8841e0e5ad585daf7e0
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-07-07 05:33:03 +0100

    bsttling with mui docs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,31 @@
 import { Autocomplete } from "@react-google-maps/api"
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@mui/material"
+import { Search } from '@mui/icons-material'
+import useStyles  from './styles'
 
 
 const Header = () => {
+
+  const classes = useStyles()
   return (
     <AppBar position="static">
-      <Toolbar className="">
-        <Typography variant="h5" className="">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
           Travel Seeker
         </Typography>
 
       <Box display='flex'>
-        <Typography variant="h6" className="">
+        <Typography variant="h6" className={classes.title}>
            Explore new places
         </Typography>
 
         <Autocomplete>
-          <div className="">
-              <div className="">
-                Search
+          <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <Search/>
               </div>
               <InputBase placeholder="Search..." classes={{
-                root: ''
+                root: classes.inputRoot, input: classes.inputInput
               }}/>
           </div>
         </Autocomplete>
