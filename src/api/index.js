767f858ff8dde8aa67eeed67eea59f92bcb5170a
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-08-02 15:15:20 +0100

    omo!

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+
+
+export const getPlaces = async () => {
+    try {
+        const res = await axios.get()
+    } catch (error) {
+        
+    }
+}
--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core'
+import PlaceDetails from '../placeDetails/PlaceDetails'
 
 import useStyles from './styles'
 
@@ -7,9 +8,22 @@ import useStyles from './styles'
 const List = () => {
   const classes = useStyles()
   const [ type, setType ] = useState('resturants')
+  const [ rating, setRating ] = useState('')
+
+  const places = [
+    {name: 'Cool place'},
+    {name: 'Fun places'},
+    {name: 'Best steak'},
+    {name: 'Cool place'},
+    {name: 'Fun places'},
+    {name: 'Best steak'},
+    {name: 'Cool place'},
+    {name: 'Fun places'},
+    {name: 'Best steak'},
+  ]
 
   return (
-    <Grid className={classes.container}>
+    <div className={classes.container}>
       <Typography variant='h6'> Resturants, Hotels and Attractions around you </Typography>
       <FormControl className={classes.formControl}>
         <InputLabel>Type</InputLabel>
@@ -21,15 +35,25 @@ const List = () => {
       </FormControl>
       
       <FormControl className={classes.formControl}>
-        <InputLabel>Type</InputLabel>
-        <Select value={type} onClick={(e)=>setType(e.target.value)}>
-          <MenuItem value='resturants'>Resturants</MenuItem>
-          <MenuItem value='hotels'>Hotels</MenuItem>
-          <MenuItem value='attractions'>Attractions</MenuItem>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onClick={(e)=>setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
         </Select>
       </FormControl>
-    </Grid>
+
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid item key={i} xs={12}>
+            <PlaceDetails place={place}/>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   )
+
 }
 
 export default List
